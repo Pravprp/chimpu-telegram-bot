88fd2893c01b8ba40425b94e8f613f14d05e0d49
pravprp <swayamprakash85649@gmail.com>
2026-03-26 18:19:50 +0530

    Update poems.py

--- poems.py
+++ poems.py
@@ -1,1 +1,14 @@
+# poems.py
 
+BANANA_POEMS = [
+    "Peela peela pyara kela,\nSab phalon mein yeh albela.\nChhilka utaro, gup-chup khao,\nSwad iska sabko batao! 🍌🎶",
+    "Bandar mama ko bhaya kela,\nDekhte hi lagaya mela.\nEk khaya, dusra khaya,\nFir pet pakad kar zor se chillaya! 🐒🍌",
+    "Aam ka raja bhale hi naam ho,\nPar kele bina sab nakaam ho.\nSubah khao ya khao sham,\nKela hi karta hai saara kaam! 🍌❤️"
+]
+
+BANANA_QUOTES = [
+    "Zindagi us kele ki tarah hai, jise agar waqt par na khaya jaye, toh woh bilkul kaala aur kharab ho jata hai. Waqt par kela khao, khush raho! 🍌✨",
+    "Pyar karna toh kela khane jaisa hai, aage badhna hai toh chhilke (ego) ko utarna hi padega! 😉🍌",
+    "Duniya gol nahi hai, duniya kele ke aakar ki hai. Isliye yahan sab thode tedhe hain! 🐒🍌",
+    "Sacha dost wahi hai jo apna aakhri kela tumhare sath share kare. 🍌🤝"
+]
