64c849ff54a948c907a898466b89d724b8d08443
pravprp <swayamprakash85649@gmail.com>
2026-03-26 18:18:27 +0530

    Update jokes.py

--- jokes.py
+++ jokes.py
@@ -1,1 +1,9 @@
+# jokes.py
 
+BANANA_JOKES = [
+    "Ek kela dusre kele se: Tu itna udas kyun hai?\nDusra kela: Kya karun bhai, main a-kela (akela) hoon na! 🍌😂",
+    "Teacher: Kela khane ke kya fayde hain?\nChimpu: Sir, kela khane ke baad chhilke par dushman ko giraya ja sakta hai! 🍌🐒",
+    "Customer: 10 rupaye mein ek kela? 5 mein de do!\nDukandar: 5 mein toh sirf chhilka milega.\nCustomer: Ye lo 5 rupaye, chhilka rakh lo aur kela mujhe de do! 🍌😎",
+    "Doctor: Roz ek kela khaya karo.\nChimpu: Doctor sahab, main kela khata nahi, kela mera dharam hai! 🍌🐵",
+    "Ek Bandar ne sadhu se pucha: Jeevan ka sabse bada sach kya hai?\nSadhu: Beta, kela chahe kitna bhi meetha ho, chhilka utarna hi padta hai! 🍌🤣"
+]
